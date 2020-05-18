4f11d2bad9e7cc753835e70b83914179eeafff92
Arthur Gomes <devarthur4718@gmail.com>
2020-05-18 15:15:20 -0300

    Splash to Login navigation.

--- app/src/main/java/com/devarthur4718/mvp/ui/Splash/SplashActivity.kt
+++ app/src/main/java/com/devarthur4718/mvp/ui/Splash/SplashActivity.kt
@@ -1,8 +1,17 @@
 package com.devarthur4718.mvp.ui.Splash
 
+import android.Manifest
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.devarthur4718.mvp.R
+import com.devarthur4718.mvp.ui.login.LoginActivity
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class SplashActivity : AppCompatActivity() {
 
@@ -15,6 +24,41 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
+        Dexter.withActivity(this)
+            .withPermissions(
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport) { /* ... */
+                    if(report.areAllPermissionsGranted()){
+
+                        openLoginActivity()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: List<PermissionRequest>,
+                    token: PermissionToken
+                ) {
+
+                }
+            }).check()
+
+    }
+
+    fun openCoreActivity(){
+        TODO("Not Implemented")
+    }
+
+    fun openLoginActivity(){
+        Handler().postDelayed({
+            startActivity(
+                Intent(this,
+                    LoginActivity::class.java)
+            )
+            finish()
+        }, SPLASH_TIME_OUT)
 
     }
 }
--- app/src/main/java/com/devarthur4718/mvp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/devarthur4718/mvp/ui/login/LoginActivity.kt
@@ -1,20 +1,23 @@
-package com.devarthur4718.mvp
+package com.devarthur4718.mvp.ui.login
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import com.devarthur4718.mvp.databinding.ActivityMainBinding
+import com.devarthur4718.mvp.R
+import com.devarthur4718.mvp.databinding.LoginActivityBinding
 import com.devarthur4718.mvp.ui.core.CoreNav
 
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding : LoginActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,
+            R.layout.login_activity
+        )
 
 
         binding.btnLogin.setOnClickListener {
