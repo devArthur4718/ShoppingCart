6216336dd501989a148a65993ddee67437481ecb
Arthur Gomes <devarthur4718@gmail.com>
2020-05-18 14:55:02 -0300

    Finished Splach activity with bg.

--- app/src/main/java/com/devarthur4718/mvp/ui/Splash/SplashActivity.kt
+++ app/src/main/java/com/devarthur4718/mvp/ui/Splash/SplashActivity.kt
@@ -6,8 +6,15 @@ import com.devarthur4718.mvp.R
 
 class SplashActivity : AppCompatActivity() {
 
+    private val SPLASH_TIME_OUT : Long =  1000L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        initViews()
+    }
+
+    private fun initViews() {
+
     }
 }
