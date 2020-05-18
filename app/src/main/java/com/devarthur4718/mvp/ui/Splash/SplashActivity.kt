1e74faf8fafa957e26d8d742e3115754221e208b
Arthur Gomes <devarthur4718@gmail.com>
2020-05-18 14:36:00 -0300

    Added Splash Activity

---
+++ app/src/main/java/com/devarthur4718/mvp/ui/Splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.devarthur4718.mvp.ui.Splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devarthur4718.mvp.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
