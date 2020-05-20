71886046dfe9a3f5aa485bb816a1ecc37c7200a4
Arthur Gomes <devarthur4718@gmail.com>
2020-05-19 22:23:02 -0300

    Seller registration flow.

--- app/src/main/java/com/devarthur4718/mvp/ui/register/BusinessContactActivity.kt
+++ app/src/main/java/com/devarthur4718/mvp/ui/register/BusinessContactActivity.kt
@@ -1,13 +1,26 @@
 package com.devarthur4718.mvp.ui.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.devarthur4718.mvp.R
+import com.devarthur4718.mvp.databinding.ActivityBusinessContactBinding
 
 class BusinessContactActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityBusinessContactBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_business_contact)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_business_contact)
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.buttonNext.setOnClickListener {
+            val intent = Intent(this,PaymentMethod::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/devarthur4718/mvp/ui/register/PaymentMethod.kt
+++ app/src/main/java/com/devarthur4718/mvp/ui/register/PaymentMethod.kt
@@ -1,13 +1,32 @@
 package com.devarthur4718.mvp.ui.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.devarthur4718.mvp.R
+import com.devarthur4718.mvp.databinding.ActivityPaymentMethodBinding
+import com.devarthur4718.mvp.ui.login.LoginActivity
+
 
 class PaymentMethod : AppCompatActivity() {
 
+    private lateinit var binding : ActivityPaymentMethodBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_payment_method)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_payment_method)
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.btnFinish.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+
+            }
+            startActivity(intent)
+            finish()
+        }
     }
 }
